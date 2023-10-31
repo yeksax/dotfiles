89b357e9e67b84291b2e06c8d23ce2cefdcffc24
Hugo Almeida <95363669+yeksax@users.noreply.github.com>
2023-10-31 17:50:55 -0300

    fix: removed dirs before linking, maybe now it works dunno

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ BASE_DIR=$PWD
 sudo cp ./pacman.conf /etc/pacman.conf
 
 # installs packages, a lot of them
-sudo pacman -S --noconfirm --needed git base-devel neovim zsh feh chromium dconf xorg lightdm lightdm-gtk-greeter i3-wm i3lock picom nodejs npm unzip neofetch scrot alsa-utils rofi github-cli noto-fonts noto-fonts-emoji noto-fonts-extra light bc jq vlc xautomation vlc playerctl pwgen ttf-font-awesome polybar ffmpeg ffmpegthumbnailer p7zip
+sudo pacman -Syyu --noconfirm --needed git base-devel neovim zsh feh chromium dconf xorg lightdm lightdm-gtk-greeter i3-wm i3lock picom nodejs npm unzip neofetch scrot alsa-utils rofi github-cli noto-fonts noto-fonts-emoji noto-fonts-extra light bc jq vlc xautomation vlc playerctl pwgen ttf-font-awesome polybar ffmpeg ffmpegthumbnailer p7zip
 
 # installs yay
 git clone https://aur.archlinux.org/yay-bin.git
@@ -16,7 +16,7 @@ yay -Y --gendb
 cd $BASE_DIR
 
 # installs gnome terminal (with transparency) and rofi-greenclip
-yay -S --noprovides --answerclean All --answerdiff None --mflags "--noconfirm --needed" rofi-greenclip gnome-terminal-transparency
+yay -S --noprovides --answerclean All --answerdiff None --mflags "--noconfirm --needed --clean" rofi-greenclip gnome-terminal-transparency
 
 # installs packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
@@ -29,12 +29,22 @@ sudo systemctl enable lightdm.service
 cd ~
 mkdir -p themes videos documents pictures downloads wallpapers .fonts .config
 
+cd ~/.config
+rm -rf i3
+rm -rf polybar
+rm -rf rofi
+rm -rf yay
+rm -rf nvim
+rm -rf dconf
+rm -rf gtk-3.0
+rm -rf picom
+
 # symlinks configs, wallpapers, configs, fonts and etc
 cd $BASE_DIR
 ln -sf ./wallpapers/* ~/wallpapers/
-ln -sf ./themes/* ~/themes/
 ln -sf ./.config/* ~/.config/
 ln -sf ./.fonts/* ~/.fonts/
+ln -sf ./themes/* ~/themes/
 
 ln -sf ./gitconfig ~/.gitconfig
 ln -sf ./zshrc ~/.zshrc
