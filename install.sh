2165ebfa5686c92e8e86da15f67df23681ebe4a7
Hugo Almeida <95363669+yeksax@users.noreply.github.com>
2023-11-03 22:22:42 -0300

    fix: exporting bun to path quero ver nao funciona agora

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 sudo pacman -S --needed --noconfirm unzip
 curl -fsSL https://bun.sh/install | bash
-alias bun=~/.bun/bin/bun
+
+export PATH=$PATH:~/.bun/bin
 
 cd i3-install
 bun install
 cd ..
 bun $(pwd)/i3-install/index.ts
+
